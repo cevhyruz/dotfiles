c05d75e4f6665b3d5d295d3cbb130516dde9bfce
devs <johnfred.fadrigalan@hotmail.com>
2018-09-15 17:16:21 +0800

    Tidy up and add displaying current playing song in status.

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -4,77 +4,77 @@
 
 # Color reference from https://jonasjacek.github.io/colors/.
 
-# Color Variables. - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Color Variables.
 status_bg=colour235
 status_fg=colour245
+song_playing='/home/devs/Projects/dotfiles/bin/current_playing_song'
 status_bg_backdrop=colour234
 status_bluegreen=colour30
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
 # Adjust window's index number if changed.
 set-option -g renumber-windows on
 
 # Remove separator between window names.
 setw -g window-status-separator ''
 
-#  Modes. - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Modes.
 setw -g clock-mode-colour $status_fg
-setw -g mode-attr italic
+setw -g mode-attr bold
 setw -g mode-fg colour13
 setw -g mode-bg colour134
 
-# Panes. - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Panes.
 set -g pane-border-bg $status_bg
 set -g pane-border-fg $status_fg
 set -g pane-active-border-bg $status_bg
 set -g pane-active-border-fg $status_fg
 
-# Statusbar. - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Statusbar.
 set -g status-position bottom
 set -g status-justify left
 set -g status-bg $status_bg
 set -g status-fg white
 set -g status-attr bold
 
-# Status Left. - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-set -g status-left-bg $status_bg
-set -g status-left-fg $status_fg
-set -g status-left  \
-" ☰#[bg=colour236,fg=colour30] #(tmux list-session | wc -l) "\
-"#[bg=colour23,fg=colour236]"\
-"#[bg=colour23,fg=colour234] #S [#(tmux list-windows | wc -l)] "\
-"#[bg=$status_bg_backdrop, fg=colour23]"\
-"#[bg=$status_bg, fg=colour234]"\
-
-# Status right. - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Status right.
+set -g status-right-length 70
 set -g status-right-bg $status_bg
 set -g status-right-fg colour239
 set -g status-right \
 "#[fg=colour235]"\
-"#[bg=colour235, fg=colour245, none] ♬ Nakapagtataka - Sponge Cola.. "\
+"#[bg=colour235, fg=colour245, none] ♬ #($song_playing).. "\
 "#[fg=colour234, bg=$status_bg]"\
 "#[fg=colour23, bg=colour234]"\
-"#[fg=colour233, bg=colour23,bold] %h %d"\
-"#[fg=colour109, bg=colour23,bold] %l:%M %p "\
+"#[fg=colour233, bg=colour23,bold] %h %d,"\
+"#[fg=colour109, bg=colour23,bold]%l:%M %p "\
 "#[fg=colour233, bg=colour29,bold] #H "
- 
-# Status right.
-set -g status-right-length 70
-set -g status-left-length 30
 
-# Current active window. - - - - - - - - - - - - - - - - - - - - - - -
+# Status Left.
+set -g status-left-length 30
+set -g status-left-bg $status_bg
+set -g status-left-fg $status_fg
+set -g status-left  \
+" ☰#[bg=colour236, fg=colour30] #(tmux list-session | wc -l) "\
+"#[bg=colour23, fg=colour236]"\
+"#[bg=colour23, fg=colour234] #S [#(tmux list-windows | wc -l)] "\
+"#[bg=$status_bg_backdrop, fg=colour23]"\
+"#[bg=$status_bg, fg=colour234]"\
+
+ 
+# Current active window.
 setw -g window-status-current-fg colour30
 setw -g window-status-current-bg $status_bg
-setw -g window-status-current-attr dim
+setw -g window-status-current-attr bold
 setw -g window-status-current-format ' #I #[fg=colour137]#W '
 
-# Normal window. - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Normal window.
 setw -g window-status-fg colour30
 setw -g window-status-bg colour235
 setw -g window-status-attr bold
 setw -g window-status-format ' #I #[fg=colour246]#W '
 
-# Messages and Command mode. - - - - - - - - - - - - - - - - - - - - -
+# Messages and Command mode.
 set -g message-attr bold
 set -g message-fg colour250
 set -g message-bg colour23
+
