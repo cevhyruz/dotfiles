bcfde39f5f2c45de49e9e546550b1148873deec6
john fred fadrigalan <johnfred.fadrigalan@hotmail.com>
2021-02-20 20:44:52 +0800

    Refactor tmux for backward compatibility

---
+++ tmux/lib/util.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+#  ---------------------------------------------------------------------------
+# File:        util.conf
+# Description: helper functions for tmux config
+# ---------------------------------------------------------------------------
+
+# get option value at runtime.
+# param: [source] [option]
+# return: option value
+function Util::get_opts() {
+  cat "${1}" | grep "${2}" | awk '{print $4}'
+}
+
+# get user option's @theme value
+# return: [@theme name]
+function Util::get_theme() {
+  localrc_path="${TMUX_LOCALRC}/local.tmuxrc";
+  opts_path="${TMUX_COMPONENT_DIR}/options.tmux"
+
+  if [[ $(Util::get_opts "${TMUX_LOCALRC}/local.tmuxrc" "@theme") ]]; then
+    THEME="$(Util::get_opts "${localrc_path}" "@theme")"
+  else
+    # default theme
+    THEME="$(Util::get_opts "${opts_path}" "@theme")"
+  fi
+
+  printf "%s" "${THEME}"
+}
+
+function Util::source_files() {
+  local FILES;
+  for FILES in $1; do
+    tmux source-file "${FILES}"
+  done;
+  unset FILES;
+}
