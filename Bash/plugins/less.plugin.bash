712df24de0a505e8bef252ca06ff91ab87464db5
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2021-05-24 16:34:27 +0800

    Add bash plugins.

--- Bash/plugins/less.plugin.bash
+++ Bash/plugins/less.plugin.bash
@@ -6,7 +6,7 @@ function main() {
   # @note: exporting LESS will pagerize some git commands and aliases,
   # set "core.pager" and/or "pager.<cmd/alias>" to false to avoid this.
   # shellcheck disable=SC2154
-  export LESS_TERMCAP_md="${yellow}" # higlight section titles in manpages
+  export LESS_TERMCAP_md="\e[33m" # higlight section titles in manpages
   export LESS="--quiet --raw-control-chars --ignore-case"
 }
 
