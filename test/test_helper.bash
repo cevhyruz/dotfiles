d68c88d46d83f4befe06d1fb167cb6aaaf077290
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2021-06-25 21:10:17 +0800

    Add vader tests in test runner.

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -2,6 +2,11 @@
 # shellcheck shell=bash
 # vi:ft=sh fdm=marker ts=2 sw=2 et
 
+# Dotfiles test suite helper file.
+#
+# Usage:
+#   load "${TEST_DIRECTORY}/test_helper.bash"
+
 load "${TEST_DEPS_DIR}/bats-support/load.bash"
 load "${TEST_DEPS_DIR}/bats-assert/load.bash"
 load "${TEST_DEPS_DIR}/bats-file/load.bash"
@@ -12,13 +17,16 @@ function _make_test_name() {
   printf "\e[1m[%s]\e[0m" "$(basename "${BATS_TEST_FILENAME}")"
 }
 
+# Usage: @test "${TEST_FILE}: this is a test" { }
 TEST_FILE="$(_make_test_name)"
 export TEST_FILE
 
 function local_setup() { true; }
+
 function local_teardown() { true; }
 
 function setup() {
+  # Usage: export LOCAL_DEP="fzf"
   if [[ -n "${LOCAL_DEP:-}" ]] &&
     ! _::command_exists "${LOCAL_DEP}"; then
     skip "Missing Dependency: ${LOCAL_DEP}"
