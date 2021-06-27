95ad331a6c82e4bf4523d19b719d39cdc79c5a69
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2021-06-28 00:34:36 +0800

    Test suite (#32)
    * Refactor test runner.
    
    * Add vader tests in test runner.
    
    * Include neovim building on CI.
    
    * Update CI.
    
    * Update CI.
    
    * another update of CI.
    
    * Update ci.
    
    * Separate bats and vimscript tests suite on CI.
    
    * Fix error.
    
    * Add CI_VIM_EXEC.
    
    * Add exec.
    
    * Experiment.
    
    * Check pwd.
    
    * Check pwd again.
    
    * Add CI checkout.
    
    * change checkout.
    
    * Add pre-commit config for vim-vint.
    
    * Add markdownlint in precommit.
    
    * Update CI.
    
    * Update CI error.

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -2,19 +2,31 @@
 # shellcheck shell=bash
 # vi:ft=sh fdm=marker ts=2 sw=2 et
 
+# Dotfiles test suite helper file.
+#
+# Usage:
+#   load "${TEST_DIRECTORY}/test_helper.bash"
+
 load "${TEST_DEPS_DIR}/bats-support/load.bash"
 load "${TEST_DEPS_DIR}/bats-assert/load.bash"
 load "${TEST_DEPS_DIR}/bats-file/load.bash"
 
 load "${TEST_DEPS_DIR}/../Bash/lib/util.bash"
 
-# shellcheck disable=SC2155
-export TEST_FILE="$(echo -e "[\e[1m$(basename "${BATS_TEST_FILENAME}")\e[0m]")"
+function _make_test_name() {
+  printf "\e[1m[%s]\e[0m" "$(basename "${BATS_TEST_FILENAME}")"
+}
+
+# Usage: @test "${TEST_FILE}: this is a test" { }
+TEST_FILE="$(_make_test_name)"
+export TEST_FILE
 
 function local_setup() { true; }
+
 function local_teardown() { true; }
 
 function setup() {
+  # Usage: export LOCAL_DEP="fzf"
   if [[ -n "${LOCAL_DEP:-}" ]] &&
     ! _::command_exists "${LOCAL_DEP}"; then
     skip "Missing Dependency: ${LOCAL_DEP}"
