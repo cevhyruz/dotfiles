b13dd0a997dcfb6837921cc31f2cacefc455aebb
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2022-01-06 14:27:13 +0800

    Cleanup nvm.plugin.bash environment.

--- Bash/plugins/nvm.plugin.bash
+++ Bash/plugins/nvm.plugin.bash
@@ -8,68 +8,63 @@
 # load nvm (Node Version Manager) if it is present.
 #
 # Globals:
-#   LAZYLOAD_NVM (default 1)
-#   set to 0 to disable loading nvm on-demand, 0 otherwise.
+#   LAZYLOAD_NVM (default: 1)
+#   set to 0 to disable lazy loading nvm, 1 otherwise
 
-function _set_nvm() {
-  # If these two functions are present, then nvm is installed.
-  if ! declare -F 'nvm' && ! declare -F 'nvm_echo'; then
-    if [[ -d "${HOME}/.nvm" ]]; then
-      export NVM_DIR="$HOME/.nvm"
-      if [[ "${LAZYLOAD_NVM:-1}" -eq 0 ]]; then
-        _load_script_files
-        return 0
-      fi
-      _lazy_load_nvm
-      return 0
-    fi
-    _set_nvm::_cleanup
-    return 1
-  fi
-}
+# ENVIRONMENT:
+# these function will be set by default if lazyloading is enabled.
+# npm, node, npm, npx.
 
-function _lazy_load_nvm() {
-  for func in nvm node npm npx; do
-    eval "${func}() {
-      #if [[ -n ${assert_call:-} ]]; then
-        #return 0
-      #fi
+function set_nvm() {
+  export NVM_DIR="${HOME}/.nvm"
+
+  if [[ -d "${NVM_DIR}" ]]; then
+    case "${LAZYLOAD_NVM:-1}" in
+    1 | true | TRUE | yes | YES)
+      lazy_load_nvm
+      echo 'lazyload'
+      ;;
+    0 | false | FALSE | no | NO)
       _load_script_files
-      ${func} \"\$@\"
-    }"
-  done
-  unset func
+      echo 'on-demand'
+      ;;
+    esac
+  fi
+
+  set_nvm::_cleanup
 }
 
-function _load_script_files() {
-  unset -f nvm node npm npx
-
-  _load_nvm_script
-  _load_nvm_bash_compl
-}
-
-function _load_nvm_script() {
+function load_script_files() {
+  # bootstrap nvm
   if command -v brew &>/dev/null &&
     [[ -s "$(brew --prefix nvm)/nvm.sh" ]]; then
     source "$(brew --prefix nvm)/nvm.sh"
   elif [[ -s "${NVM_DIR}/nvm.sh" ]]; then
     source "${NVM_DIR}/nvm.sh"
   fi
-}
-
-function _load_nvm_bash_compl() {
+  # load completion
   if [[ -f "${NVM_DIR}/bash_completion" ]]; then
     source "${NVM_DIR}/bash_completion"
   fi
 }
 
-function _set_nvm::_cleanup() {
-  unset -f _set_nvm \
-    _lazy_load_nvm \
-    _load_script_files \
-    _load_nvm_script \
-    _load_nvm_bash_compl \
-    _set_nvm::_cleanup
+function lazy_load_nvm() {
+  for func in nvm node npm npx; do
+    eval "function ${func}() {
+       unset -f nvm node npm npx;
+       $(declare -f load_script_files | tail +3 | head)
+      ${func} \"\$@\"
+    }"
+  done
+  unset func
 }
 
-_set_nvm
+function set_nvm::_cleanup() {
+  unset -f \
+    set_nvm \
+    lazy_load_nvm \
+    set_nvm::_cleanup \
+    load_script_files
+}
+
+set_nvm
