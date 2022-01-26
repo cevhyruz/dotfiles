74809f4386130dc43571c48a39571a64903a7585
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2022-01-26 11:54:25 +0800

    Refactor.

--- Bash/lib/theme.bash
+++ Bash/lib/theme.bash
@@ -2,54 +2,14 @@
 # shellcheck shell=bash disable=SC2034,SC2086,SC2145
 # vim: ft=sh fdm=marker ts=2 sw=2 et
 
-function __git_branch () {
-  git symbolic-ref --quiet --short HEAD 2> /dev/null \
-    || git rev-parse --short HEAD 2> /dev/null \
-    || echo "unknown"
-}
-
-function __has_modified () {
-  if ! git diff-files --quiet --ignore-submodules --; then
-    return 0;
-  fi;
-  return 1
-}
-function __has_staged () {
-  if ! git diff --quiet --ignore-submodules --cached; then
-    return 0;
-  fi;
-  return 1
-}
-
-function __has_stash () {
-  if git rev-parse --verify refs/stash &> /dev/null; then
-    return 0;
-  fi;
-  return 1
-}
-function __has_untracked () {
-  if [[ -n "$(git ls-files --others --exclude-standard)" ]]; then
-    return 0;
-  fi;
-  return 1
-}
-function __in_git_dir () {
-  if [[ $(git rev-parse --is-inside-git-dir 2> /dev/null) == 'true' ]]; then
-    return 0;
-  fi;
-  return 1
-}
-
-function __in_git_repo () {
-  if ! git rev-parse &> /dev/null; then
-    return 1;
-  fi
-}
-
-function __load_ansi_attrib () {
+function main() {
+  reset_bold="\e[22m"
+  reset_underline="\e[24m"
+  reset_reverse="\e[27m"
   defaultfg="\e[39m";
   defaultbg="\e[49m";
-  reset="\e[0m";
+  reset="\e[0m"
+  resetall="\e[0m${reset_bold}${reset_underline}${reset_reverse}";
   bold="\e[1m";
   dim="\e[2m";
   italic="\e[3m";
@@ -58,10 +18,66 @@ function __load_ansi_attrib () {
   reverse="\e[7m";
   hidden="\e[8m";
   strikethrough="\e[9m";
-  normal="${defaultfg}${defaultbg}";
+  normal="\e[38;5;247m";
   curvyline="${underline::-1}:${italic:3}";
   overline="\e[53m";
   double_underline="${underline::-1}:${dim:3}"
+}
+
+function __prompt_user_at_host() {
+  if [[ -n "${SSH_TTY:-}" ]]; then
+    printf "%b" "${resetall}${bold}${fg_red}"
+  else
+    printf "%b" "${resetall}${bold}${fg_white}"
+  fi
+}
+
+function __prompt_return_exit_code () {
+  if [[ "${EXIT_CODE:-}" -ne 0 ]]; then
+    printf "%b" "${resetall}${dim}   [exited ${EXIT_CODE}]"
+  fi
+}
+
+function __exit_color() {
+  if [[ "${EXIT_CODE:-}" -eq 0 ]]; then
+    printf "%b" "${resetall}${bold}${fg_green}"
+  else
+    printf "%b" "${resetall}${bold}${fg_red}"
+  fi
+}
+
+function __git_prompt() {
+  local status="    "
+
+  ! git rev-parse &> /dev/null && return
+  if [[ $(git rev-parse --is-inside-git-dir 2> /dev/null) == 'false' ]]; then
+    git update-index --really-refresh -q &> /dev/null
+    if ! git diff --quiet --ignore-submodules --cached; then
+      status="${status/ /+}"
+    fi
+    if ! git diff-files --quiet --ignore-submodules --; then
+      status="${status/ /!}"
+    fi
+    if [[ -n "$(git ls-files --others --exclude-standard)" ]]; then
+      status="${status/ /?}"
+    fi
+    if git rev-parse --verify refs/stash &> /dev/null; then
+      status="${status/ /*}"
+    fi
+  fi
+  local -a git_prompt=(
+    "${reset}"
+    "${1:-${bold}${fg_red}}"
+    "$(git symbolic-ref --quiet --short HEAD 2> /dev/null \
+    || git rev-parse --short HEAD 2> /dev/null \
+    || echo "unknown")"
+    "${reset}"
+    " "
+    "${2:-${bold}${fg_yellow}}"
+    "${status:-}"
+    "${reset}"
+  );
+  printf "%b" "${git_prompt[@]}"
 }
 
 function __load_default_pallete () {
@@ -75,17 +91,6 @@ function __load_default_pallete () {
   bg_white="\e[47m"   fg_white="\e[37m"
 }
 
-function __make_dircolors () {
-  LS_COLORS=''
-  local -r dircolors="${1}"
-  set -o noglob
-  for entry in ${dircolors[*]}; do
-    LS_COLORS+="$entry";
-  done;
-  set +o noglob
-  export LS_COLORS
-}
-
 function __make_dircolors_pallete () {
   local -ar __lscolors=(
     '__reset'         '__bg_black'   '__fg_black'
@@ -97,27 +102,36 @@ function __make_dircolors_pallete () {
     '__underline'     '__bg_cyan'    '__fg_cyan'
     '__strikethrough' '__bg_white'   '__fg_white'
     '__dim'
-  );
-
-  local -r __normal="${defaultfg:3:-1};${defaultbg:3:-1}";
+    '__normal' )
 
   for color in ${__lscolors[*]}; do
-    eval "local -r ${color}=$(echo -E \"\$\{${color:2}:3:-1\}\")";
-  done;
+    eval "local -r ${color}=$(echo -E \"\$\{${color:2}:3:-1\}\")"
+  done
 
-  __load_dircolors
+  local -ar dircolors=(
+    "no=${__normal}:"              # NORMAL, NORM
+    "fi=${__normal}:"              # FILE
+    "di=${__dim};${__normal};${__fg_cyan}:" # DIR
+    "ln=${__italic};${__fg_blue}:" # SYMLINK, LINK, LNK
+    "or=${__italic};${__fg_red}:"  # ORPHAN
+    "mi=${__fg_red}:"              # MISSING
+    "bd=${__fg_blue}:"             # BLOCK, BLK
+    "cd=${__fg_red}:"              # CHAR, CHR
+    "ex=${__reset};${__fg_green}:" # EXEC
+    "do=${__normal}:"              # DOOR
+    "so=${__normal}:"              # SOCK
+    "pi=${__normal}:"              # FIFO, PIPE
+    "sg=${__normal}:"              # SETGID
+    "su=${__normal}:"              # SETUID
+    "st=${__normal}:"              # STICKY
+    "tw=${__normal}:"              # STICKY_OTHER_WRITABLE
+    "ow=${__fg_magenta}:" )        # OTHER_WRITABLE
+
+  LS_COLORS=''
+  for entry in ${dircolors[*]}; do
+    LS_COLORS+="$entry"
+  done
+  export LS_COLORS
 }
 
-function __make_rgb_colors_from_pallete() {
-  local -ar __lscolors=(
-    'reset'         'bg_black'   'fg_black'
-    'bold'          'bg_red'     'fg_red'
-    'italic'        'bg_green'   'fg_green'
-    'reverse'       'bg_yellow'  'fg_yellow'
-    'defaultfg'     'bg_blue'    'fg_blue'
-    'defaultbg'     'bg_magenta' 'fg_magenta'
-    'underline'     'bg_cyan'    'fg_cyan'
-    'strikethrough' 'bg_white'   'fg_white'
-    'dim'
-  );
-}
+main && unset -f main
--- test/Bash/hooks.test.bats
+++ test/Bash/hooks.test.bats
@@ -1,25 +1,12 @@
 #!/usr/bin/env bats
 # vi:ft=bats fdm=marker ts=2 sw=2 et
 
-inside_bats=1
-
 load "${TEST_DIRECTORY}/test_helper.bash"
-load "Bash/lib/hooks.bash"
+load "Bash/core/hooks.bash"
 
-@test "${TEST_FILE} __pre_command should return if '\$AT_PROMPT' has not been set." {
-  AT_PROMPT=''
+@test "${TEST_FILE} __pre_command will return if '\$AT_PROMPT' is unset." {
+  unset AT_PROMPT
   run __pre_command
-  assert_failure 1
-}
-
-@test "${TEST_FILE} __post_command should return if '\$FIRST_PROMPT' has been set." {
-  FIRST_PROMPT=1
-  run __post_command
-  assert_failure 1
-}
-
-@test "${TEST_FILE} __run_hook should fail if no argument." {
-  run __run_hook ''
   assert_failure 1
 }
 
@@ -31,6 +18,12 @@ load "Bash/lib/hooks.bash"
   assert_line --index 0 "one"
   assert_line --index 1 "two"
   assert_success
+}
+
+@test "${TEST_FILE} __post_command should return if '\$FIRST_PROMPT' has been set." {
+  FIRST_PROMPT=1
+  run __post_command
+  assert_failure 1
 }
 
 @test "${TEST_FILE} __post_command should run '\$POST_COMMAND in order or precedence'" {
