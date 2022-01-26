acf9d7d19860a81d179b13e2a88f8957dfc93f21
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2022-01-26 11:51:25 +0800

    Remove init files and  rename files.

---
+++ Bash/core/hooks.bash
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+# shellcheck shell=bash disable=SC2034
+# vim: ft=sh fdm=marker ts=2 sw=2 et
+#
+# Execute an array or commands delimited by ';'
+# before and after the shell command.
+#
+# Globals:
+# PRE_COMMAND  : commands to be run before the command.
+# POST_COMMAND : commands to be run after the command.
+# EXIT_CODE    : exit status of the given command.
+
+function __pre_command() {
+  if [[ -z "${AT_PROMPT:-}" ]]; then
+    return 1
+  fi
+  unset AT_PROMPT
+  eval "${PRE_COMMAND[@]:-}"
+}
+
+function __post_command() {
+  declare -g EXIT_CODE="$1"
+
+  AT_PROMPT=1
+
+  if [[ -n "${FIRST_PROMPT:-}" ]]; then
+    unset FIRST_PROMPT
+    return 1
+  fi
+  eval "${POST_COMMAND[@]:-}"
+}
+
+function __init_hooks() {
+  declare -a POST_COMMAND=()
+  declare -a PRE_COMMAND=()
+
+  # avoid __post_command pre-firing before the first prompt.
+  FIRST_PROMPT=1
+
+  trap '__pre_command' DEBUG
+
+  # fireup __post_command after every prompt
+  # and pass previous command return status.
+  PROMPT_COMMAND+=('__post_command "$?"')
+}
+
+# FIXME: For some reason, bash trap triggers an error
+# when inside bats. We'll skip initializing for now.
+if [[ -x "${BATS_EXECUTABLE:-}" ]]; then
+  __init_hooks \
+    && unset -f __init_hooks
+fi
---
+++ Bash/themes/default.theme.bash
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+# shellcheck shell=bash disable=SC2034,SC2154,SC2016
+# vi: ft=sh fdm=marker ts=2 sw=2 et
+#
+# Default bash prompt theme when BASH_PROMPT_THEME is not set or is empty.
+
+function main() {
+  if [[ "${COLORTERM}" =~ ^(truecolor|24bit)$ ]]; then
+    bg_black="\e[48;5;234m"  fg_black="\e[38;5;234m"
+    bg_red="\e[48;5;203m"    fg_red="\e[38;5;203m"
+    bg_green="\e[48;5;35m"   fg_green="\e[38;5;35m"
+    bg_yellow="\e[48;5;221m" fg_yellow="\e[38;5;221m"
+    bg_blue="\e[34m"         fg_blue="\e[34m"
+    bg_magenta="\e[35m"      fg_magenta="\e[35m"
+    bg_cyan="\e[36m"         fg_cyan="\e[36m"
+    bg_white="\e[37m"        fg_white="\e[37m"
+  else
+    __load_default_pallete
+  fi
+
+  __make_dircolors_pallete
+
+  # set ps1
+  PS1="\[${reset}${bold}${normal}\]"
+  local -a ps1=(
+    "\n"
+    "\$(__exit_color)"
+    "╭─"
+    " "
+    "\$(__prompt_user_at_host)"
+    "\u@\H"
+    ":"
+    "\[${fg_cyan}\]\w"
+    " "
+    "\$(__git_prompt)"
+    "\$(__prompt_return_exit_code)"
+    "\[${reset}\]"
+    "\n"
+    "\$(__exit_color)"
+    "╰➤"
+    "${reset}"
+    "${dim} $: ${resetall}"
+    "\[\e[38;5;216m\]") # LightSalmon1
+  PS1+="$(printf "%s" "${ps1[@]}")"
+
+  # change command output color.
+  PRE_COMMAND+=('printf "%b" "${normal}";')
+}
+
+main && unset -f main
