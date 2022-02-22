f08fd2a4c26873b67148874b0a62e47e38eedd5d
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2022-02-22 22:30:57 +0800

    Fix: pyenv shims directory appear multiple times in PATH.
    https://github.com/pyenv/pyenv/issues/1649

--- Bash/plugins/pyenv.plugin.bash
+++ Bash/plugins/pyenv.plugin.bash
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 # shellcheck shell=bash
-# vi:ft=sh fdm=marker ts=2 sw=2 et
+# vim: ft=sh fdm=marker ts=2 sw=2 et
 
 # Pyenv plugin for Bash
 
-PYENV_ROOT="${HOME}/.pyenv"
+export PYENV_ROOT="${HOME}/.pyenv"
 
 if _::add_to_path "${PYENV_ROOT}/bin"; then
   if command -v pyenv 1>/dev/null 2>&1; then
-    eval "$(pyenv init --path)"
+    echo 'test'
+    # basically: eval "$(pyenv init --path)"
+    _::add_to_path "${PYENV_ROOT}/shims"
   fi
   # Build with dynamic python library enabled.
   # some program needed this (eg: YouCompleteMe)
