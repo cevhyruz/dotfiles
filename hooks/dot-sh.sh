56e3abc51b07ca342592cf9b513f19ebf3e9bed0
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2022-03-09 19:11:25 +0800

    add format for custom hooks.

--- hooks/dot-sh.sh
+++ hooks/dot-sh.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 
 exit_code=0
+
+function _print_error() {
+  printf "%b" " \e[38;5;238m┃\e[0m $1 \n"
+  exit_code=1
+}
+
 for file in "$@"; do
+
+  printf "%b" "\n\e[38;5;203m${file}\e[0m\n"
+
   # Confirm file is executable
-  #
   if [[ ! -x "${file}" ]]; then
-    echo "Bash file \`${file}\` is not executable"
-    exit_code=1
+    _print_error ".sh file should be only executables"
   fi
 
-  # Confirm expected #! header
-  #
+  # Confirm expected shebang header
   LINE1="$(head -n 1 "${file}")"
   if [[ "${LINE1}" != "#!/usr/bin/env bash" ]]; then
-    echo "Bash file \`${file}\` has bad/missing #! header"
-    exit_code=1
+    _print_error "bad/missing shebang"
   fi
 done
 
