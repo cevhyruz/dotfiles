e7d120f065ef02cfa6f300ddcef99a2af7467cbb
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2022-06-26 13:19:48 +0800

    [nvim-tree] Remove deprecated options.

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -67,10 +67,6 @@ nvim_tree.setup {
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = false,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true,
-  },
   diagnostics = {
     enable = true,
     icons = {
@@ -101,7 +97,6 @@ nvim_tree.setup {
     height = 30,
     hide_root_folder = false,
     side = "left",
-    auto_resize = true,
     preserve_window_proportions = true,
     mappings = {
       custom_only = false,
