bfc8e71a922cee1aac32f9d0d8c68b04274c65d8
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2022-07-03 00:17:44 +0800

    [vscode] Reconfigure vscode to be vim-like.

--- vscode/install.sh
+++ vscode/install.sh
@@ -6,8 +6,15 @@ declare -a extensions=(
   'vscodevim.vim'
   'malmaud.tmux'
   'shan.code-settings-sync'
+  'ryuta46.multi-command'
+  'emmanuelbeziat.vscode-great-icons'
+  'pkief.material-icon-theme'
 )
 
-for extension in "${extensions[@]}"; do
-  command code --install-extension "${extension}"
-done
+function install() {
+  for extension in "${extensions[@]}"; do
+    command code --install-extension "$@" "${extension}"
+  done
+}
+
+install "$@"
