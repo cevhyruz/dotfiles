fdee5d8baea557c5b2c77c3b8ee87676b3cefe4f
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2022-07-07 14:10:29 +0800

    [vscode] Vim-like keybindings and sensible settings.

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,20 +1,46 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2155
 #
 # VScode plugin installer.
 
+declare -a installed=()
+declare -a failed=()
+
 declare -a extensions=(
-  'vscodevim.vim'
+  # syntax highlighting
   'malmaud.tmux'
-  'shan.code-settings-sync'
+
+  # utilities
+  'vscodevim.vim'
+  'Shan.code-settings-sync'
   'ryuta46.multi-command'
+  'stlr.scroll-with-stationary-cursor'
+
+  # visual aesthetics
   'emmanuelbeziat.vscode-great-icons'
-  'pkief.material-icon-theme'
+  'PKief.material-icon-theme'
+
+  # linters
+  'timonwong.shellcheck'
+  'sumneko.lua'
 )
 
-function install() {
+function install_plugin() {
+  echo -e "Installing..\n"
   for extension in "${extensions[@]}"; do
-    command code --install-extension "$@" "${extension}"
+    if command code --install-extension "${extension}" &> /dev/null; then
+       printf  "%b\n" " \e[38;5;35m[\u2713]\e[0m ${extension}"
+       installed+=("${extension}")
+     else
+       printf  "%b\n" " \e[38;5;203m[\u2715]\e[0m ${extension}"
+       failed+=("${extension}")
+    fi
   done
+  echo -e "\n${#installed[@]} plugin installed, ${#failed[@]} failed."
+  echo -e "\nTo update all plugins, run: ${BASH_SOURCE[0]} --force"
+  if [[ "${#failed}" -gt 0 ]]; then
+    return 1
+  fi
 }
 
-install "$@"
+install_plugin "$@"
