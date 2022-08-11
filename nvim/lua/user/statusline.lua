193f731e61f74fc9e68ceab83e2e5146a6b79887
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2022-08-11 23:05:20 +0800

    [nvim] Add current git branch in statusline.

--- nvim/lua/user/statusline.lua
+++ nvim/lua/user/statusline.lua
@@ -1,52 +1,4 @@
 Statusline = {}
- 
-local modes = {
-  ["n"] = "NORMAL",
-  ["no"] = "NORMAL",
-  ["v"] = "VISUAL",
-  ["V"] = "VISUAL LINE",
-  [""] = "VISUAL BLOCK",
-  ["s"] = "SELECT",
-  ["S"] = "SELECT LINE",
-  [""] = "SELECT BLOCK",
-  ["i"] = "INSERT",
-  ["ic"] = "INSERT",
-  ["R"] = "REPLACE",
-  ["Rv"] = "VISUAL REPLACE",
-  ["c"] = "COMMAND",
-  ["cv"] = "VIM EX",
-  ["ce"] = "EX",
-  ["r"] = "PROMPT",
-  ["rm"] = "MOAR",
-  ["r?"] = "CONFIRM",
-  ["!"] = "SHELL",
-  ["t"] = "TERMINAL",
-  ["nt"] = "TERMINAL"
-}
-
-local function mode()
-  local current_mode = vim.api.nvim_get_mode().mode
-  return string.format(" %s ", modes[current_mode]):upper()
-end
-
-local function update_mode_colors()
-  local current_mode = vim.api.nvim_get_mode().mode
-  local mode_color = "%#StatusLineAccent#"
-  if current_mode == "n" then
-      mode_color = "%#StatuslineAccent#"
-  elseif current_mode == "i" or current_mode == "ic" then
-      mode_color = "%#StatuslineInsertAccent#"
-  elseif current_mode == "v" or current_mode == "V" or current_mode == "" then
-      mode_color = "%#StatuslineVisualAccent#"
-  elseif current_mode == "R" then
-      mode_color = "%#StatuslineReplaceAccent#"
-  elseif current_mode == "c" then
-      mode_color = "%#StatuslineCmdLineAccent#"
-  elseif current_mode == "t" then
-      mode_color = "%#StatuslineTerminalAccent#"
-  end
-  return mode_color
-end
 
 local function filepath()
   local fpath = vim.fn.fnamemodify(vim.fn.expand "%", ":~:.:h")
@@ -62,7 +14,7 @@ local function filename()
   if fname == "" then
       return ""
   end
-  return fname .. " "
+  return " " .. fname .. " "
 end
 
 local function lsp()
@@ -96,7 +48,7 @@ local function lsp()
     info = " %#LspDiagnosticsSignInformation# " .. count["info"]
   end
 
-  return errors .. warnings .. hints .. info .. "%#Normal#"
+  return errors .. warnings .. hints .. info .. "%#WildMenu#"
 end
 
 local function filetype()
@@ -110,15 +62,14 @@ local function lineinfo()
   return " %P %l:%c "
 end
 
+
 Statusline.active = function()
   return table.concat {
     "%#Statusline#",
-    update_mode_colors(),
-    mode(),
-    "%#Normal# ",
-    filepath(),
+    vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'"),
     filename(),
-    "%#Normal#",
+    "%#Comment#",
+    filepath(),
     lsp(),
     "%=%#StatusLineExtra#",
     filetype(),
@@ -126,27 +77,14 @@ Statusline.active = function()
   }
 end
 
-function Statusline.inactive()
-  return " %F"
-end
-
-function Statusline.short()
-  return "%#StatusLineNC#   NvimTree"
-end
-
-
 local group = vim.api.nvim_create_augroup('statuslineEvents', { clear = true })
 
 vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
   group = group,
   callback = function()
-    vim.opt_local.statusline = Statusline.active()
+    vim.opt_global.statusline = Statusline.active()
   end
 })
 
-vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
-  group = group,
-  callback = function()
-    vim.opt_local.statusline = Statusline.inactive()
-  end
-})
+vim.o.laststatus = 3
+vim.o.showmode = true
