ab7226600055d5d2ce5001f25378726c0eefacee
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2023-01-29 19:05:10 +0800

    [Bash][plugin] sane clipboard plugin

---
+++ src/Bash/plugins/clipboard.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# shellcheck shell=bash
+# vim: ft=sh fdm=marker ts=2 sw=2 et
+#
+# Sane clipboard configuration
+
+alias pbcopy="xsel --clipboard"
+alias pbpaste="xsel --clipboard"
+
+# readline functions
+bind -m vi-command -x '"\C-x\C-y": printf "%s" "${READLINE_LINE-}" | pbcopy'
+
+main
