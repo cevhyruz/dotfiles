2111f8b0b7fe5b1d6f786a36c0cde7f7c6bb89e0
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2023-02-04 19:02:38 +0800

    [Bash][plugins] add rbenv

---
+++ src/Bash/plugins/rbenv.plugin.bash
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# shellcheck shell=bash source=/dev/null disable=SC2162
+# vim: ft=sh fdm=marker ts=2 sw=2 et
+#
+# Manage your app ruby's environment.
+
+export RBENV_ROOT="${HOME}/Projects/cloned-repo/rbenv"
+
+_::add_to_path "${RBENV_ROOT}/shims"
+_::add_to_path "${RBENV_ROOT}/bin"
+
+function rbenv() {
+  local command="${1:-}"
+
+  if [[ "$#" -eq 0 ]]; then
+    echo "Note: to upgrade 'ruby-build' plugin"
+    echo "run 'rbenv-rb-update' function.\n"
+  else
+    shift
+  fi
+
+  case "$command" in
+    rehash|shell) eval "$(rbenv "sh-$command" "$@")";;
+    *) command rbenv "$command" "$@";;
+  esac
+}
+
+function rbenv-rb-update() {
+  command git -C "${RBENV_ROOT}"/plugins/ruby-build pull
+}
